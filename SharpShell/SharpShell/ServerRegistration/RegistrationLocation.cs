7b4b1c720c9f7853449b6f12fcb45c2cb525c566
Tony Valenti <Tony.Valenti@GMail.com>
2017-10-12 09:21:10 -0500

    Please commit your "RegistrationLocation.cs" file.  It is missing from the solution.

---
+++ SharpShell/SharpShell/ServerRegistration/RegistrationLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShell.ServerRegistration
+{
+    public enum RegistrationLocation
+    {
+        UserSid,
+        CurrentUserPseudoSid,
+        LocalMachinePseudoSid,
+    }
+}
